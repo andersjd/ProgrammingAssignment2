3ffbd1989e914a093707fc9368d1cf2056e336e3
JD Anderson <andersjd@sbcglobal.net>
2014-08-24 08:21:24 -0400

    Added Code and Comments for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a special case of a matrix which will cache it's inverse once calculated
+## so that subsequent calls can pull from the cache instead of re-calculating
+##
+## Note that this only supports matrices that are invertible
 
-## Write a short comment describing this function
 
+## Function: makeCacheMatrix
+##
+## Description: Creates a special case matrix object that can also contains a cached inverse
+##
+## Parameters:
+##    x:  A matrix that is invertible
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL  ## cached inverse
+  
+  ## On Set: store the matrix and clear the cache
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ## On Get: return the matrix
+  get <- function() x
+  
+  ## On setinvers: store the inverse in the cache
+  setinverse <- function(inverse) m <<- inverse
+  
+  ## On getinverse: return the cache
+  getinverse <- function() m
+  
+  ## Return list of methods
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Function: cacheSolve
+##
+## Description: Calculates the inverse of a makeCacheMatrix, returning the cache if present
+##
+## Parameters:
+##    x:  An instance of makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Get and check the cache, returning if available
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  ## Cache was not available, perform the solve and set the cache
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+## EOF
